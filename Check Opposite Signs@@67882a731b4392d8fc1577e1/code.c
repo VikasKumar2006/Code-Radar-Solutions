1069afb779129b9ff561067102b2e5df9b1eee32
2410991777 <vikas1777.becse24@chitkara.edu.in>
2025-01-27 09:18:50 +0530

    Solution to Check Opposite Signs

--- Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
+++ Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
@@ -2,7 +2,8 @@
  int main(){
     int a,b;
     scanf("%d %d",&a,&b);
-    if(a>=0 && b<=0){
+    //if(a>=0 && b<=0){ #wrong
+    if((a^b)<0){
         printf("True");
     }
     else{
@@ -10,3 +11,5 @@
     }
     return 0;
  }
+//The bitwise XOR (^) operation compares the sign bits of a and b. If their signs are different, the result will have the sign bit set (i.e., negative).
+//This method is more efficient and avoids multiplication.
