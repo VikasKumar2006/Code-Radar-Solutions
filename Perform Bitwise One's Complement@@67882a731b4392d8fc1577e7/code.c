8c9fcdf299a7a0d73b90ae9ab49b7f516a0b7b50
2410991777 <vikas1777.becse24@chitkara.edu.in>
2025-01-27 10:04:02 +0530

    Solution to Perform Bitwise One's Complement

--- Perform Bitwise One's Complement@@67882a731b4392d8fc1577e7/code.c
+++ Perform Bitwise One's Complement@@67882a731b4392d8fc1577e7/code.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int num = 5;  // Binary representation: 0000 0101
-    int result = ~num;  // Apply bitwise one's complement
-    printf("%d\n", num);
-    printf("%d\n", result);
+    int num1 = 5;  // First test case: input 5
+    int num2 = 0;  // Second test case: input 0
+
+    int result1 = ~num1;  // Apply one's complement
+    int result2 = ~num2;  // Apply one's complement
+
+    // Print results
+    printf("Test case 1 - Input: %d, Output: %d\n", num1, result1);
+    printf("Test case 2 - Input: %d, Output: %d\n", num2, result2);
+    
     return 0;
 }
+
