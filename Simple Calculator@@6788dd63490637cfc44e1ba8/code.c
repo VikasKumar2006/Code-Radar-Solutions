a47164024c81fd1ffa2a87742eb50ee629c54f2d
2410991777 <vikas1777.becse24@chitkara.edu.in>
2025-02-08 00:03:00 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,18 +1,40 @@
+// #include <stdio.h>
+// int main(){
+//     int a,b;
+//     char c;
+//     scanf("%d %d %c",&a,&b,&c);
+//     if(c== '+'){
+//         printf("%d",a+b);
+//     }else if(c== '-'){
+//         printf("%d",a-b);
+//     }else if(c== '*'){
+//         printf("%d",a*b);
+//     }else if(c== '/'){
+//         printf("%d",a/b);
+//     }else{
+//         printf("error");
+//     }
+//     return 0;
+// }
 #include <stdio.h>
-int main(){
-    int a,b;
+
+int main() {
+    int a, b;
     char c;
-    scanf("%d %d %c",&a,&b,&c);
-    if(c== '+'){
-        printf("%d",a+b);
-    }else if(c== '-'){
-        printf("%d",a-b);
-    }else if(c== '*'){
-        printf("%d",a*b);
-    }else if(c== '/'){
-        printf("%d",a/b);
-    }else{
-        printf("error");
+    
+    scanf("%d %d %c", &a, &b, &c);
+
+    if (c == '+') {
+        printf("%d\n", a + b);
+    } else if (c == '-') {
+        printf("%d\n", a - b);
+    } else if (c == '*') {
+        printf("%d\n", a * b);
+    } else if (c == '/' && b != 0) {
+        printf("%d\n", a / b);
+    } else {
+        printf("error\n");
     }
+
     return 0;
 }
